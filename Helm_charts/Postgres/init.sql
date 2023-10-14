785e8ebe2eddc07e69a5723ce2165fdd1c91cee2
N4si <chaudharinasi@gmail.com>
2023-10-14 14:07:08 +0530

    Update

---
+++ Helm_charts/Postgres/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE auth_user (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR (255) NOT NULL,
+    password VARCHAR (255) NOT NULL
+);
+
+--Add Username and Password for Admin User
+INSERT INTO auth_user (email, password) VALUES ('chaudharinasi@gmail.com', '123456');
